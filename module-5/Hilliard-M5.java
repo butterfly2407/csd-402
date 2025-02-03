928d351ff8d476664904013baf07f4e3cb6aeeca
katieh <katiehilliard24@gmail.com>
2025-02-02 22:55:50 -0500

    Added Hilliard-M5.java to module 5

---
+++ module-5/Hilliard-M5.java
@@ -0,0 +1,92 @@
+//  Katie Hilliard, 02/02/2025, M4 Assignment
+
+public class ArrayLocator {
+
+    public static int[] locateLargest(double[][] arrayParam) {
+        int[] location = {0, 0};
+        double max = arrayParam[0][0];
+
+        for (int i = 0; i < arrayParam.length; i++) {
+            for (int j = 0; j < arrayParam[i].length; j++) {
+                if (arrayParam[i][j] > max) {
+                    max = arrayParam[i][j];
+                    location[0] = i;
+                    location[1] = j;
+                }
+            }
+        }
+        return location;
+    }
+
+    public static int[] locateLargest(int[][] arrayParam) {
+        int[] location = {0, 0};
+        int max = arrayParam[0][0];
+
+        for (int i = 0; i < arrayParam.length; i++) {
+            for (int j = 0; j < arrayParam[i].length; j++) {
+                if (arrayParam[i][j] > max) {
+                    max = arrayParam[i][j];
+                    location[0] = i;
+                    location[1] = j;
+                }
+            }
+        }
+        return location;
+    }
+
+    public static int[] locateSmallest(double[][] arrayParam) {
+        int[] location = {0, 0};
+        double min = arrayParam[0][0];
+
+        for (int i = 0; i < arrayParam.length; i++) {
+            for (int j = 0; j < arrayParam[i].length; j++) {
+                if (arrayParam[i][j] < min) {
+                    min = arrayParam[i][j];
+                    location[0] = i;
+                    location[1] = j;
+                }
+            }
+        }
+        return location;
+    }
+
+    public static int[] locateSmallest(int[][] arrayParam) {
+        int[] location = {0, 0};
+        int min = arrayParam[0][0];
+
+        for (int i = 0; i < arrayParam.length; i++) {
+            for (int j = 0; j < arrayParam[i].length; j++) {
+                if (arrayParam[i][j] < min) {
+                    min = arrayParam[i][j];
+                    location[0] = i;
+                    location[1] = j;
+                }
+            }
+        }
+        return location;
+    }
+
+    public static void main(String[] args) {
+        int[][] intArray = {
+            {3, 8, 5},
+            {12, 1, 7},
+            {6, 4, 9}
+        };
+
+        double[][] doubleArray = {
+            {2.5, 3.1, 5.6},
+            {1.2, 0.9, 4.3},
+            {6.7, 8.2, 2.1}
+        };
+
+        int[] largestInt = locateLargest(intArray);
+        int[] smallestInt = locateSmallest(intArray);
+        int[] largestDouble = locateLargest(doubleArray);
+        int[] smallestDouble = locateSmallest(doubleArray);
+
+        System.out.println("Largest in int array at: [" + largestInt[0] + ", " + largestInt[1] + "]");
+        System.out.println("Smallest in int array at: [" + smallestInt[0] + ", " + smallestInt[1] + "]");
+        System.out.println("Largest in double array at: [" + largestDouble[0] + ", " + largestDouble[1] + "]");
+        System.out.println("Smallest in double array at: [" + smallestDouble[0] + ", " + smallestDouble[1] + "]");
+    }
+}
